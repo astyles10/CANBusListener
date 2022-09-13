5cac5e17cb9b21e2bc6aa470e2c891ac83ba57f5
astyles <astyles@ats-t1.com>
2022-09-13 12:05:14 +1000

    Updates

--- receive_check/receive_check.ino
+++ receive_check/receive_check.ino
@@ -7,7 +7,9 @@
 #include <SPI.h>
 
 #include <iterator>
+// #include <algorithm>
 #include <map>
+// #include <vector>
 
 #include "df_can.h"
 
@@ -16,47 +18,72 @@ typedef struct options {
   options() = default;
 } options;
 
-const int SPI_CS_PIN = 10;
-MCPCAN CAN(SPI_CS_PIN);  // Set CS pin
+enum SerialCommands { LIST_IDS = 'd', SELECT_ID = 's', TOGGLE_LOGGING = 'l' };
 
+const int SPI_CS_PIN = 10;
+MCPCAN CAN(SPI_CS_PIN);
 std::map<uint32_t, bool> CANIdMap;
+// FILE logFile;
+// std::vector<uint32_t> CANIds;
+static uint32_t selectedCANId = 0;
+
 void setup() {
   Serial.begin(115200);
-  int count = 50;  // the max numbers of initializint the CAN-BUS, if initialize
-                   // failed first!.
-  do {
-    CAN.init();  // must initialize the Can interface here!
-    if (CAN_OK == CAN.begin(CAN_500KBPS))  // init can bus : baudrate = 500k
-    {
-      Serial.println("DFROBOT's CAN BUS Shield init ok!");
-      break;
-    } else {
-      Serial.println("DFROBOT's CAN BUS Shield init fail");
-      Serial.println("Please Init CAN BUS Shield again");
-
-      delay(100);
-      if (count <= 1)
-        Serial.println("Please give up trying!, trying is useless!");
-    }
-  } while (count--);
+  connectToCANBus();
 }
 
 void loop() {
+  handleCommandReceive();
+  handleCanMessage();
+}
+
+void connectToCANBus() {
+  while (true) {
+    Serial.println("Attempting to connect to CAN BUS...");
+    CAN.init();
+    if (CAN_OK == CAN.begin(CAN_500KBPS)) {
+      Serial.println("CAN BUS Shield init ok!");
+      break;
+    } else {
+      delay(1000);
+    }
+  };
+}
+
+void handleCommandReceive() {
+  if (Serial.available() > 0) {
+    int command = Serial.read();
+    switch (command) {
+      case LIST_IDS:
+        listKnownCanIds();
+        break;
+      case SELECT_ID:
+        SelectActiveCanId();
+        break;
+      case TOGGLE_LOGGING:
+        break;
+      default:
+        break;
+    }
+    Serial.print("Received command: ");
+    Serial.println(Serial.read());
+  }
+}
+
+void handleCanMessage() {
   unsigned char len = 0;
   unsigned char buf[8];
-
-  handleCommandReceive();
-
   if (CAN_MSGAVAIL == CAN.checkReceive()) {
     if (CANIdMap.find(CAN.getCanId()) == CANIdMap.end()) {
+    // if (std::find(CANIds.begin(), CANIds.end(), CAN.getCanId())) {
       Serial.print("Found new CAN ID: ");
       Serial.println(CAN.getCanId(), HEX);
       CANIdMap.insert({CAN.getCanId(), false});
+      // CANIds.push_back(CAN.getCanId());
     }
-    CAN.readMsgBuf(&len, buf);  // read data,  len: data length, buf: data buf
 
-    for (int i = 0; i < len; i++)  // print the data
-    {
+    CAN.readMsgBuf(&len, buf);
+    for (int i = 0; i < len; i++) {
       Serial.print(buf[i], HEX);
       Serial.print("\t");
     }
@@ -64,9 +91,24 @@ void loop() {
   }
 }
 
-void handleCommandReceive() {
-  if (Serial.available() > 0) {
-    Serial.print("Received command: ");
-    Serial.println(Serial.read());
+void listKnownCanIds() {
+  Serial.println("List of known CAN ids:");
+  for (const auto& canId : CANIdMap) {
+    Serial.println(canId.first);
   }
+}
+
+void SelectActiveCanId() {
+  Serial.println("Select a CAN ID:");
+  for (const auto& canId : CANIdMap) {
+    Serial.println(canId.first);
+  }
+  while (!Serial.available()) {
+    // wait for input
+  }
+  char buffer[8];
+  size_t len;
+  Serial.readBytesUntil('\n', buffer, len);
+  
+  // Serial.readString()
 }
